c28f6203d472802654050fea73b6a378e1acb2fb
wishesmandragora <54888885+wishesmandragora@users.noreply.github.com>
2020-09-01 19:50:38 +0200

    Initialized git repository with starter files.

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo get_bloginfo( 'name' ); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    
