e159812a9c160d6d5784c07799b2ab843fcb766a
wishesmandragora <anabudimir98@gmail.com>
2021-02-06 12:25:37 +0100

    Added theme setup files for functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+
+require get_template_directory_uri() . 'functions/theme-setup/basic.php';
+
+
+require get_template_directory_uri() . 'functions/theme-setup/styles.php';
+
+
+require get_template_directory_uri() . 'functions/theme-setup/scripts.php';
