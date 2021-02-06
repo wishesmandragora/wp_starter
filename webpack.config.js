e0694e55e3cf30636b36bf35896bb94d7d311649
wishesmandragora <anabudimir98@gmail.com>
2021-02-06 12:54:19 +0100

    Added new features for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,79 @@
-// Webpack uses this to work with directories
 const path = require('path');
+const glob = require('glob');
 
-// This is the main configuration object.
-// Here you write different options and tell Webpack what to do
+// include the js minification plugin
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+// include the css extraction and minification plugins
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const PATHS = {
+  src: path.join(__dirname, 'assets')
+}
+const assets = glob.sync('./assets/*/*.*');
+let entries =  assets.reduce(function(obj, el){
+         
+  let elObj = path.parse(el);
+
+  if(elObj.base !== 'app.scss'  && elObj.base !=='dashboard.scss') {
+
+    obj[elObj.name] = [el];
+
+  }
+  return obj;
+},{})
+
+ entries.frontend.push('./assets/sass/app.scss');
+ entries.admin.push('./assets/sass/dashboard.scss');
+
 module.exports = {
 
-  // Path to your entry point. From this file Webpack will begin his work
-  entry: './assets/js/index.js',
-
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
+  entry: entries,
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'js/[name]-build.js',
   },
-
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript 
-  // minifying and other thing so let's set mode to development
-  mode: 'development'
+  module: {
+    rules: [
+      // perform js babelization on all .js files
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['babel-preset-env']
+         }
+        }
+      },
+      // compile all .scss files to plain old css
+      {
+        test: /\.(sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    // extract css into dedicated file
+    new MiniCssExtractPlugin({
+      filename: '/css/[name].min.css'
+    }),
+    // new PurgecssPlugin({
+    //   paths: glob.sync(path.join(__dirname, '.php')),
+    // })
+  ],
+  optimization: {
+    minimizer: [
+      // enable the js minification plugin
+      new UglifyJSPlugin({
+        cache: true,
+        parallel: true
+      }),
+      // enable the css minification plugin
+      new OptimizeCSSAssetsPlugin({
+        
+      })
+    ]
+  }
 };
